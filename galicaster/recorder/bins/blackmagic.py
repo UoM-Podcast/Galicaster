5ad8b6b7777f4d75c16c192ca88ebbbb45aeadba
androidwiltron <andrew.j.wilson7@googlemail.com>
2015-06-18 09:14:26 +0100

    change blackmagic bin to not output images for peakaboo

--- galicaster/recorder/bins/blackmagic.py
+++ galicaster/recorder/bins/blackmagic.py
@@ -29,9 +29,10 @@ videostr = ( ' decklinksrc connection=sdi mode=12 name=gc-blackmagic-src ! '
              #REC VIDEO
              ' gc-blackmagic-tee. ! queue ! valve drop=false name=gc-blackmagic-valve ! ffmpegcolorspace ! '
              ' gc-blackmagic-enc ! queue ! gc-blackmagic-muxer ! '
-             ' queue ! identity name=gc-blackmagic-idend ! filesink name=gc-blackmagic-sink async=false' 
-             ' gc-blackmagic-tee. ! queue ! videorate ! videoscale !'
-             ' jpegenc ! multifilesink name=gc-blackmagic-thumbsink ')
+             ' queue ! identity name=gc-blackmagic-idend ! filesink name=gc-blackmagic-sink async=false'
+             )
+             # ' gc-blackmagic-tee. ! queue ! videorate ! videoscale !'
+             # ' jpegenc ! multifilesink name=gc-blackmagic-thumbsink ')
 audiostr= (
             #AUDIO
             ' gc-blackmagic-src.audiosrc ! identity name=gc-blackmagic-idaudio ! queue ! '
@@ -285,7 +286,7 @@ class GCblackmagic(gst.Bin, base.Base):
                 else:
                     text.set_property(opts, vals)
 
-        self.set_value_in_pipeline(path.join('/tmp', self.options['file'] + '.jpg'), 'gc-blackmagic-thumbsink', 'location')
+        # self.set_value_in_pipeline(path.join('/tmp', self.options['file'] + '.jpg'), 'gc-blackmagic-thumbsink', 'location')
 
 
   def changeValve(self, value):
