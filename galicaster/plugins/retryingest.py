c250f0e00a1711067b8628099ce2ea8f2269c300
andrew wilson <wilson@manchester.ac.uk>
2017-01-12 16:57:52 +0000

    CAG-73 port retryingest plugin from 1.3.x-uom

--- galicaster/plugins/retryingest.py
+++ galicaster/plugins/retryingest.py
@@ -31,23 +31,64 @@ conf = context.get_conf()
 occlient = context.get_occlient()
 repo = context.get_repository()
 
+check_ingested = conf.get_boolean('retryingest', 'check_ingested')
 check_published = conf.get_boolean('retryingest', 'check_published')
 check_after = conf.get_int('retryingest', 'check_after') or 300
 check_nightly = conf.get_boolean('retryingest', 'nightly')
+check_state = conf.get_boolean('retryingest', 'check_state')
 last_checked = time.time()
 
 
-
 def init():
+    logger.debug('check_ingested set to {}'.format(check_ingested))
     logger.debug('check_published set to {}'.format(check_published))
     logger.debug('check_after set to {}'.format(str(check_after)))
     logger.debug('check_nightly set to {}'.format(check_nightly))
+    logger.debug('check_state set to {}'.format(check_state))
 
     try:
         dispatcher = context.get_dispatcher()
         dispatcher.connect('timer-short', reingest)
     except ValueError:
         pass
+
+
+def has_ingested(mp_id, mp):
+    # check if the mediapackage has been ingested
+    search_result = occlient.search_by_mp_workflow_id(mp_id)['workflow']['operations']['operation'][2]
+    if search_result['id'] == 'ingest' and search_result['state'] == 'SUCCEEDED':
+        logger.debug('mediapackage %s has already been ingested', mp_id)
+        # mediapackage with workflow id has actually been ingested successfully at some point
+        mp.setOpStatus('ingest', mediapackage.OP_DONE)
+        repo.update(mp)
+        return True
+    logger.debug('mediapackage %s has not been ingested', mp_id)
+    return False
+
+
+def has_succeeded(mp_id, mp):
+    #check if the mediapackage has the workflow.status file
+    try:
+        mp_workflow_state = mp.getAttachment('workflow.status')
+    except:
+        mp_workflow_state = None
+    if mp_workflow_state:
+        logger.debug('mp {} already marked as failed workflow - Ignoring'.format(mp_id))
+    else:
+        if mp.getOpStatus('ingest') == mediapackage.OP_DONE or mp.getOpStatus('ingest') == mediapackage.OP_NIGHTLY:
+            # check for the current mediapackage workflow state
+            try:
+                workflow_state = occlient.search_by_mp_workflow_id(mp_id)['workflow']['state']
+            except IOError:
+                return True
+            logger.debug('mp {0}. mhorn state: {1}'.format(mp_id, workflow_state))
+            if workflow_state == 'FAILED':
+                logger.info('mp {} : Workflow Failed'.format(mp_id))
+                mp.addAttachmentAsString('#' + workflow_state, 'workflow.status', False, 'workflow.status')
+                mp.setOpStatus('ingest', mediapackage.OP_FAILED)
+                repo.update(mp)
+                return False
+    return True
 
 
 def is_published(mp_id, mp):
@@ -73,19 +114,41 @@ def reingest(sender=None):
 
     worker = context.get_worker()
     for mp_id, mp in repo.iteritems():
-        logger.debug('reingest checking: {0} status: {1}'.format(mp_id,
-                                                                 mediapackage.op_status[mp.getOpStatus('ingest')]))
+        logger.debug('reingest checking: {0} status: {1}'.format(mp_id, mediapackage.op_status[mp.getOpStatus('ingest')]))
         # only finished recordings
-        if not (mp.status == mediapackage.SCHEDULED or mp.status == mediapackage.RECORDING):
-            if mp.getOpStatus('ingest') == mediapackage.OP_FAILED:
-                # check mediapackage status on opencast if needed
-                if (check_published and not is_published(mp_id, mp)) or not check_published:
-                    # postpone the ingest until the 'nightly' ingest time else ingest immediately
-                    if check_nightly:
-                        logger.info('scheduled nightly reingest of failed mediapackage: {}'.format(mp_id))
-                        mp.setOpStatus("ingest", mediapackage.OP_NIGHTLY)
-                        repo.update(mp)
-                    else:
-                        logger.info('Starting reingest of failed mediapackage: {}'.format(mp_id))
-                        worker.enqueue_job_by_name('ingest',mp)
+        if mp.status not in [mediapackage.SCHEDULED, mediapackage.RECORDING]:
+
+            if check_ingested or check_state:
+                # make sure the mediapackage is not manual before checking the workflow state
+                # OC will not know the GC mpid given
+                if mp.getOCCaptureAgentProperties() != {}:
+                    try:
+                        if check_state and not has_succeeded(mp_id, mp):
+                            logger.info('Set mediapackage to Failed: Failed Workflow: %s', mp_id)
+                    except RuntimeError as e:
+                        logger.debug('matterhorn ' + str(e))
+                    logger.debug('reingest checking: %s status: %s', mp_id, mediapackage.op_status[mp.getOpStatus('ingest')])
+                if mp.getOpStatus('ingest') == mediapackage.OP_FAILED or mp.getOpStatus('ingest') == mediapackage.OP_IDLE:
+                    # check mediapackage status on matterhorn if needed
+                    if (check_ingested and not has_ingested(mp_id, mp)) or not check_ingested:
+                        if check_nightly:
+                            logger.info('scheduled nightly reingest of failed mediapackage: {}'.format(mp_id))
+                            mp.setOpStatus("ingest", mediapackage.OP_NIGHTLY)
+                            repo.update(mp)
+                        else:
+                            logger.info('Starting reingest of failed mediapackage: {}'.format(mp_id))
+                            worker.enqueue_job_by_name('ingest', mp)
+
+            if check_published:
+                if mp.getOpStatus('ingest') == mediapackage.OP_FAILED:
+                    # check mediapackage status on opencast if needed
+                    if (check_published and not is_published(mp_id, mp)) or not check_published:
+                        # postpone the ingest until the 'nightly' ingest time else ingest immediately
+                        if check_nightly:
+                            logger.info('scheduled nightly reingest of failed mediapackage: {}'.format(mp_id))
+                            mp.setOpStatus("ingest", mediapackage.OP_NIGHTLY)
+                            repo.update(mp)
+                        else:
+                            logger.info('Starting reingest of failed mediapackage: {}'.format(mp_id))
+                            worker.enqueue_job_by_name('ingest',mp)
     last_checked = time.time()
