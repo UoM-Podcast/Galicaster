edb20b4fdf89ad9efc42f6fb8f69c3d4146749f4
androidwiltron <andrew.j.wilson7@googlemail.com>
2017-08-07 11:36:58 +0100

    CAG-134 fix to the camera-pc time drift which causes onvif commands to fail. implemented setting datetime via VAPIX api to force correct this at each galicaster restart

--- galicaster/utils/camctrl_http_interface.py
+++ galicaster/utils/camctrl_http_interface.py
@@ -1,6 +1,7 @@
 import requests
 from requests.auth import HTTPDigestAuth
 import logging
+import datetime
 
 logging.getLogger("requests").setLevel(logging.WARNING)
 
@@ -12,8 +13,8 @@ class AxisWeb(object):
         self.led_state = "off"
         # On init, make sure LED is off
         self.tallyled(False)
-        # Make sure the time is set to PC
-        self.settime("PC")
+        # Make sure the time is set
+        self.vapix_settime()
 
     def sendcommand(self, fullurl, data):
         headers = {'X-Requested-Auth': 'Digest'}
@@ -25,7 +26,17 @@ class AxisWeb(object):
             print 'ERROR when accessing axis http interface: ' + e
         return r
 
+    def send_vapix_command(self, fullurl):
+        headers = {'X-Requested-Auth': 'Digest'}
+        try:
+
+            r = requests.get(fullurl, headers=headers, auth=HTTPDigestAuth(self.cam_username, self.cam_password))
+        except Exception as e:
+            print 'ERROR when accessing axis http interface: ' + e
+        return r
+
     def tallyled(self, switch=False):
+        # this uses the 'plain text config, section of the axis web config, technically this isnt supported api
         if switch:
             self.led_state = "on"
         else:
@@ -36,8 +47,28 @@ class AxisWeb(object):
         return snd_cmd.status_code
 
     def settime(self, version="PC"):
+        # this uses the 'plain text config, section of the axis web config, technically this isnt supported api
         url = 'http://' + self.cam_hostname + '/sm/sm.srv'
         postfield = {"root_Time_SyncSource": version,
                      "return_page": "/admin/config.shtml?menu=&submenu=&group=Time", "action": "modify"}
         snd_cmd = self.sendcommand(url, postfield)
         return snd_cmd.status_code
+
+    def vapix_gettime(self):
+        url = 'http://' + self.cam_hostname + '/axis-cgi/date.cgi?action=get'
+        snd_vpx_cmd = self.send_vapix_command(url)
+        return snd_vpx_cmd.status_code
+
+    def vapix_settime(self):
+        now = datetime.datetime.now()
+        yr = now.year
+        mth = now.month
+        dy = now.day
+        hr = now.hour
+        mn = now.minute
+        sc = now.second
+        time_action = '/axis-cgi/date.cgi?action=set&' \
+                      'year={}&month={}&day={}&hour={}&minute={}&second={}'.format(yr, mth, dy, hr, mn, sc)
+        url = 'http://' + self.cam_hostname + time_action
+        snd_vpx_cmd = self.send_vapix_command(url)
+        return snd_vpx_cmd.status_code
