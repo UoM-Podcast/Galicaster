31df347123eff8b5ea9583eab3ac9bbeafb73dff
androidwiltron <andrew.j.wilson7@googlemail.com>
2017-09-15 17:55:01 +0100

    changes, removing 2 opencast calls as init, setting the inital state and getting series

--- galicaster/opencast/service.py
+++ galicaster/opencast/service.py
@@ -187,9 +187,9 @@ class OCService(object):
         try:
             self.client.welcome()
             self.__set_opencast_up()
-            self.jobs.put((self.set_state, ()))
+            # self.jobs.put((self.set_state, ()))
             self.jobs.put((self.process_ical, ()))
-            self.jobs.put((self.update_series,()))
+            # self.jobs.put((self.update_series,()))
             if self.conf.tracks_visible_to_opencast():
                 self.logger.info('Be careful using profiles and opencast scheduler')
         except Exception as exc:
